584ccbdacf54cff6a71839fe4f0bd970d3fb15c9
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-10-19 18:29:51 +0100

    chronometer

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,59 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0; 
   }
   startClick(callback) {
-    // ... your code goes here
-  }
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+
+      if(callback) {
+        callback();
+      };
+
+    }, 1000); 
+  };
+
   getMinutes() {
-    // ... your code goes here
+    if(this.currentTime === 0) {
+      return 0;
+    }
+
+    return this.currentTime = Math.floor(this.currentTime / 60);
+    
   }
+
   getSeconds() {
-    // ... your code goes here
+    if(this.currentTime === 0) {
+      return 0;
+    }
+
+    return this.currentTime % 60;
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  
+  twoDigitsNumber(number) {
+
+      if (number < 10) {
+        return `0${number}`;
+      } 
+    return `${number}`
   }
+    
   stopClick() {
-    // ... your code goes here
+
+    clearInterval(this.intervalId);
   }
+
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+      let min = this.getMinutes();
+      let sec = this.getSeconds();
+      if (min < 10) {
+        return `${0}${min}:${0}${sec}`;
+      } 
+      
+      return `${min}:${sec}`;
   }
 }
